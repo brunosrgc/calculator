65a0a04818efba3aff854dda919b4d4d618a02b4
brunosrgc <brunosergiocosta5@gmail.com>
2024-03-19 18:43:49 -0300

    Creating Division() and Multiplication()

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Subtraction();
+            Multiplication();
         }
         static void Soma()
         {
@@ -31,12 +31,38 @@ namespace Calculator
             Console.Write("Second Value: ");
             float v2 = float.Parse(Console.ReadLine());
 
-            float result = v1-v2;
+            float resultSub = v1 - v2;
             Console.WriteLine("");
-            Console.WriteLine("The result of subtraction is: " + result);
+            Console.WriteLine("The result of subtraction is: " + resultSub);
+            Console.ReadKey();
+        }
+
+        static void Division()
+        {
+            Console.Clear();
+            Console.Write("First Value: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.Write("Second Value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultDiv = v1 / v2;
+            Console.Write("");
+            Console.WriteLine("The result of division is: " + resultDiv);
+        }
+
+        static void Multiplication()
+        {
+            Console.Clear();
+            Console.Write("First Value: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.Write("Second Value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float resultMulti = v1 * v2;
+            Console.WriteLine("");
+            Console.WriteLine("The result of multiplication is: " + resultMulti);
             Console.ReadKey();
         }
     }
-    
 }
 
--- obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b4edf97f062bf4e534a100dd07d2355cf4384e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4057486bfb440044f007fe3db6fa43efa89134e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
