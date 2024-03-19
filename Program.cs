b4057486bfb440044f007fe3db6fa43efa89134e
brunosrgc <brunosergiocosta5@gmail.com>
2024-03-19 18:25:53 -0300

    creating Subtraction

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Soma();
+            Subtraction();
         }
         static void Soma()
         {
@@ -19,8 +19,24 @@ namespace Calculator
 
             Console.WriteLine("");
             float resultSum = v1 + v2;
-            Console.WriteLine("The result of the sum is: " + resultSum); 
+            Console.WriteLine("The result of the sum is: " + resultSum);
+            Console.ReadKey();
+        }
+
+        static void Subtraction()
+        {
+            Console.Clear();
+            Console.Write("First Value: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.Write("Second Value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            float result = v1-v2;
+            Console.WriteLine("");
+            Console.WriteLine("The result of subtraction is: " + result);
+            Console.ReadKey();
         }
     }
+    
 }
 
