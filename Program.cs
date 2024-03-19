0b4edf97f062bf4e534a100dd07d2355cf4384e1
brunosrgc <brunosergiocosta5@gmail.com>
2024-03-19 18:15:53 -0300

    creating Sum

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Soma();
+        }
+        static void Soma()
+        {
+            Console.Clear();
+
+            Console.Write("First Value: ");
+            float v1 = float.Parse(Console.ReadLine());
+            Console.Write("Second Value: ");
+            float v2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            float resultSum = v1 + v2;
+            Console.WriteLine("The result of the sum is: " + resultSum); 
         }
     }
 }
